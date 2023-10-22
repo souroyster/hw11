6d6730fbd9aa480e611218b7da38ac100b906972
souroyster <valter.oleksandra@gmail.com>
2023-10-22 19:02:14 +0200

    Update hw11.py

--- hw11.py
+++ hw11.py
@@ -3,69 +3,86 @@ from collections import UserDict
 
 class Field:
     def __init__(self, value):
-        self.value = value
+        self._value = value
+
+    def get_value(self):
+        return self._value
+
+    def set_value(self, value):
+        self._value = value
 
     def __str__(self):
-        return str(self.value)
+        return str(self._value)
 
 class Name(Field):
     pass
 
 class Phone(Field):
-    def __init__(self, value):
+    def get_value(self):
+        return self._value
+
+    def set_value(self, value):
         if not isinstance(value, str):
             raise ValueError("Phone number must be a string")
         if not value.isdigit() or len(value) != 10:
             raise ValueError("Invalid phone number format")
-        super().__init__(value)
+        self._value = value
 
 class Birthday(Field):
-    def __init__(self, value):
+    def get_value(self):
+        return self._value
+
+    def set_value(self, value):
         try:
             datetime.strptime(value, '%Y-%m-%d')
         except ValueError:
             raise ValueError("Invalid date format, please use YYYY-MM-DD")
-        super().__init__(value)
+        self._value = value
+
 
 class Record:
     def __init__(self, name, birthday=None):
-        self.name = Name(name)
-        self.phones = []
-        self.birthday = Birthday(birthday) if birthday else None
+        self._name = Name(name)
+        self._phones = []
+        self._birthday = Birthday(birthday) if birthday else None
 
     def add_phone(self, phone):
-        self.phones.append(Phone(phone))
+        phone = Phone(phone)
+        self._phones.append(phone)
 
     def remove_phone(self, phone):
-        self.phones = [p for p in self.phones if p.value != phone]
+        self._phones = [p for p in self._phones if p.get_value() != phone]
 
     def edit_phone(self, old_phone, new_phone):
-        if self.find_phone(old_phone) is None:
+        if not any(old_phone == p.get_value() for p in self._phones):
             raise ValueError(f"Phone number '{old_phone}' does not exist.")
         self.remove_phone(old_phone)
         self.add_phone(new_phone)
 
     def find_phone(self, phone):
-        for p in self.phones:
-            if p.value == phone:
+        for p in self._phones:
+            if p.get_value() == phone:
                 return p
 
     def days_to_birthday(self):
-        if not self.birthday:
+        if not self._birthday:
             return None
         today = datetime.today()
-        next_birthday = datetime(today.year, self.birthday.value.month, self.birthday.value.day)
+        next_birthday = datetime(today.year, int(self._birthday.get_value().split('-')[1]),
+                                int(self._birthday.get_value().split('-')[2]))
         if next_birthday < today:
-            next_birthday = datetime(today.year + 1, self.birthday.value.month, self.birthday.value.day)
+            next_birthday = datetime(today.year + 1, int(self._birthday.get_value().split('-')[1]),
+                                    int(self._birthday.get_value().split('-')[2]))
         return (next_birthday - today).days
 
     def __str__(self):
-        phones = '; '.join(p.value for p in self.phones)
-        return f"Contact name: {self.name.value}, phones: {phones}, birthday: {self.birthday.value if self.birthday else 'N/A'}"
+        phones = '; '.join(p.get_value() for p in self._phones)
+        return f"Contact name: {self._name.get_value()}, phones: {phones}, birthday: {self._birthday.get_value() if self._birthday else 'N/A'}"
+
 
 class AddressBook(UserDict):
     def add_record(self, record):
-        self.data[record.name.value] = record
+        self.data[record._name.get_value()] = record
 
     def find(self, name):
         return self.data.get(name)
@@ -75,8 +92,8 @@ class AddressBook(UserDict):
             del self.data[name]
         else:
             print(f"Contact '{name}' does not exist in the address book.")
-        
+
     def iterator(self, N):
         records = list(self.data.values())
         for i in range(0, len(records), N):
-            yield records[i:i+N]
+            yield records[i:i + N]
